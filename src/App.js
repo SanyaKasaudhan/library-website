e9e7a5aa108bf6224089fb003b359d37f4559ea3
SANYA KASAUDHAN <48054000+SanyaKasaudhan@users.noreply.github.com>
2022-10-04 19:12:03 +0530

    cards styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Cards from "./components/Cards";
 import Navbar from "./components/Navbar";
+ import Test from "./components/Test"
 function App() {
   return (
     <div className="App">
       <Navbar />
      <Cards />
+     <Test />
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -20,10 +20,10 @@ const Cards = () => {
             user.map((data) =>{
             return(
                 <>
-               <div className=" bg-white rounded-lg border max-w-xs border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
-               style={{width:"18 rem"}}>
+               <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 m-2 bg-white rounded-lg border max-w-xs border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+            >
     <a href="#" className=''>
-        <img className="rounded-t-lg min-w-full  max-h-72" src={data.imageLink} alt="" />
+        <img className="rounded-t-lg min-w-full max-w-32 max-h-72" src={data.imageLink} alt="" />
     </a>
     <div class="px-5 pb-5">
         <a href="#">
@@ -38,8 +38,8 @@ const Cards = () => {
             <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">{data.language}</span>
         </div>
         <div class="flex justify-between items-center">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white">
-              {data.pages}</span>
+            <span class="text-xl font-bold text-gray-900 dark:text-white">
+              {data.pages} pages</span>
             <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
         </div>
     </div>

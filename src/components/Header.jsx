7fedae57e1a34863ec0fe9707608238e0ce595b1
SANYA KASAUDHAN <48054000+SanyaKasaudhan@users.noreply.github.com>
2022-10-07 12:00:31 +0530

    svg images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import book from '../book.png'
+
+import { faTruckFast } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import delivery from '../delivery.webp'
 const Header = () => {
   return (
     <div>
-           <h1 className='uppercase text-pink-500 text-5xl text-center m-5'>create  your future with us!</h1>
-         
-        <div className="container flex flex-row">
-            <div className="content1 ">
-                </div>
-            <div className="content2">
-                <img src={book} alt="" />
+        <h1 className='uppercase text-orange-400 text-5xl text-center m-5'>create  your future with us!</h1>
+         <h3 className='uppercase text-orange-300 text-3xl text-center '>Find your dream book here</h3>
+       
+            <figure className="flex justify-center">
+                <img src={delivery} alt="" className='min-w-fit'/>
+            </figure>
+          <div className="content">
+            <h2 className='capitalize text-center text-2xl text-amber-500'>Why Choose library?</h2>
+          </div>
+
+          <div className='delivery-box'>
+            <div className="box1">
+            <i class="fa-light fa-truck-fast"></i>
+            <FontAwesomeIcon icon="faTruckFast" />
             </div>
-        </div>
+          </div>
 
 
     </div>
